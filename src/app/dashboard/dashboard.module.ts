d7151dc98871ec0a22ee3917e4cae482bfd39554
gabrielfirmo <gabriel_firmo@hotmail.com>
2021-05-28 23:09:20 -0300

    Integração com api

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { StockCardComponent } from './stock-card/stock-card.component';
-
-
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,8 @@ import { StockCardComponent } from './stock-card/stock-card.component';
     StockCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ]
 })
 export class DashboardModule { }
